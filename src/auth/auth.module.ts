5ed8d9825839157d8e38c145036445994b7f9efb
Timur <timengalytchev@mail.ru>
2024-06-26 12:41:11 +0300

    made simply auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+        private readonly users = [
+            {
+                userId: 1,
+                username: 'Timur',
+                password: '123'
+            },
+            {
+                userId: 2,
+                username: 'Timur',
+                password: '123'
+            }
+        ];
+    
+    async findOne(username: string): Promise<User | undefined>{
+        return this.users.find(user => user.username === username)
+    }
+}
