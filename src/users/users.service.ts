f37d5e2e0c9deededd1627738159f9e0d165b681
Timur <timengalytchev@mail.ru>
2024-06-26 17:06:08 +0300

    docker compose file

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
-        private readonly users = [
-            {
-                userId: 1,
-                username: 'Timur',
-                password: '123'
-            },
-            {
-                userId: 2,
-                username: 'Timur',
-                password: '123'
-            }
-        ];
+     constructor(private prisma: PrismaService) {}
+     
+    //     private readonly users = [
+    //         {
+    //             userId: 1,
+    //             username: 'Timur',
+    //             password: '123'
+    //         },
+    //         {
+    //             userId: 2,
+    //             username: 'Timur',
+    //             password: '123'
+    //         }
+    //     ];
     
-    async findOne(username: string): Promise<User | undefined>{
-        return this.users.find(user => user.username === username)
+    // async findOne(username: string): Promise<User | undefined>{
+    //     return this.users.find(user => user.username === username)
+    // }
+
+    async findOne(username: string): Promise<User | null> {
+        
+        try {
+            const user = await this.prisma.user.findFirst({
+                where: {
+                    username: {
+                        equals: username,
+                    },
+                },
+            });
+            return user;
+        } catch (error) {
+            console.error('Error finding user:', error);
+            return null;
+        }
     }
 }
